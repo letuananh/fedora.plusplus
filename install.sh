bcbb5bccfa6e5c68ec44fadc23f550ccc8d9de27
Le Tuan Anh <tuananh.ke@gmail.com>
2014-10-24 14:26:49 +0800

    Create bin folder

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 TEMPLATE_DIR=~/Templates
 FONT_DIR=~/.fonts
-
+BIN_DIR=~/bin
 # Install templates
 if [ ! -d "$TEMPLATE_DIR" ]; then
     echo "Creating template folder"
@@ -10,6 +10,15 @@ else
 fi
 echo "Copying templates to home folder"
 cp ./res/Templates/* "$TEMPLATE_DIR" -vu
+
+# Create bin folder
+if [ ! -d "${BIN_DIR}" ]; then
+    echo "Creating bin folder"
+    mkdir "${BIN_DIR}" -v
+else
+    echo "Bin folder exists"
+fi
+# TODO: Add it to PATH
 
 # Install fonts
 if [ ! -d "$FONT_DIR" ]; then
