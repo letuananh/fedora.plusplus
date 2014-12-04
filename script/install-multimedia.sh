b58e5415d0ab27843718ea310306cba669999894
Le Tuan Anh <tuananh.ke@gmail.com>
2014-12-04 16:14:13 +0700

    Add pinta install script

--- script/install-multimedia.sh
+++ script/install-multimedia.sh
@@ -2,4 +2,4 @@
 
 yum install -y gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer-ffmpeg vlc musescore tuxguitar gimp inkscape darktable audio-convert-mod lame mac faac faad easytag kid3-qt ffmpeg
 # A nice light-weight image editor
-yum install pinta
+yum install -y pinta
