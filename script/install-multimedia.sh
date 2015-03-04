f21707c55c875681d76005d035aa9db2c15ca31a
tuananh <tuananh@localhost.localdomain>
2015-03-04 09:48:16 +0700

    Update script

--- script/install-multimedia.sh
+++ script/install-multimedia.sh
@@ -2,4 +2,5 @@
 
 yum install -y gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer-ffmpeg vlc musescore tuxguitar gimp inkscape darktable audio-convert-mod lame mac faac faad easytag kid3-qt ffmpeg
 # A nice light-weight image editor
-yum install -y pinta
+yum install -y pinta firefox
+yum install -y alsa-lib.i686
