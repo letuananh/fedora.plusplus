13bbdb41bec327c30dd3e4f03f9f8c91f55643a0
Le Tuan Anh <tuananh.ke@gmail.com>
2017-02-11 11:53:21 +0800

    Add banshee

--- script/install-multimedia.sh
+++ script/install-multimedia.sh
@@ -3,6 +3,9 @@
 # Photography, multimedia
 yum install -y gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer-ffmpeg vlc gimp inkscape darktable audio-convert-mod lame mac faac faad easytag kid3-qt ffmpeg nomacs
 
+# to support banshee with MP3 playback
+dnf install -y banshee gstreamer1-plugins-ugly
+
 # Musical software
 yum install -y musescore tuxguitar 
 
